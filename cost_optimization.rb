7c65c6b62fd23f37e600773b323f535cd882b619
Rakesh verma <rakesh.verma0487@gmail.com>
2014-01-14 23:04:35 +0530

    delta upgradation pheromone logic for ant-colony algorithm :smile:

--- cost_optimization.rb
+++ cost_optimization.rb
@@ -12,4 +12,14 @@ def calculate_ouptput(a, b, c)
 	a.to_i + b.to_i + c.to_i
 end
 
-get_inputs
+def time_cost_quality_changed_pheromone(q, value_of_objective)
+	q.to_f/(3*value_of_objective.to_f)
+end
+
+def risk_changed_pheromone(time, risk)
+	1.0/value_of_objective(time.to_f, risk.to_f)
+end
+
+def value_of_objective(time, risk)
+	((time/10)**risk)/0.25
+end
